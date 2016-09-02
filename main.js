7d6a5944c15c056f3fea0ef03c41d1fcbdaea2f5
camilin87 <camilin87@gmail.com>
2016-09-02 08:08:48 -0400

    add some input capturing code

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Forwarder");
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line){
+    console.log("C: " + line);
+})
+
+rl.on('close', function(line){
+    console.log("C_CLOSE");
+})
+
+console.log("C_AWS_REGION", process.env.AWS_REGION)
