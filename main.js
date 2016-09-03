510cbea5748fe8ea14e5e7bebd293bb9e5aa549a
camilin87 <camilin87@gmail.com>
2016-09-03 14:57:20 -0400

    the locks didn't work

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var readline = require('readline');
 var Rx = require('rx');
+var promise = require("the-promise-factory");
+
+var Lock = require('lock')
+var lock = Lock();
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -14,15 +18,18 @@ var lines = Rx.Observable.fromEvent(rl, 'line')
     .skipWhile((b) => (b || []).length === 0);
 
 lines.subscribe(
-    (lines) => console.log("FF: " + lines.map(l => l.toString()).join(",").substr(0, 20)),
+    (lines) => {
+        // lock("aws-transmission", (release) => {
+            // promise.create((fulfill, reject) => {
+                console.log("FF: " + lines.map(l => l.toString()).join(",").substr(0, 20))
+        //         fulfill();
+        //     }).then(() => {
+        //         release();
+        //     })
+        // })
+    },
     err => console.log("FF_Error: %s", err),
     () => console.log("FF_COMPLETED")
 );
-
-// lines.forEach(
-//     (lines) => console.log("FF: " + lines.map(l => l.toString()).join(",").substr(0, 20)),
-//     err => console.log("FF_Error: %s", err),
-//     () => console.log("FF_COMPLETED")
-// );
 
 console.log("FF_AWS_REGION", process.env.AWS_REGION)
