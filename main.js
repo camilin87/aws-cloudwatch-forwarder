57c657de289eb516dddb642ee4decb941ce2b231
camilin87 <camilin87@gmail.com>
2016-09-03 16:01:56 -0400

    fuck rx... I'll try a poor mans approach...

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 var readline = require('readline');
-var Rx = require('rx');
-var promise = require("the-promise-factory");
-
-var Lock = require('lock')
-var lock = Lock();
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -11,25 +6,10 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-var lines = Rx.Observable.fromEvent(rl, 'line')
-    .takeUntil(Rx.Observable.fromEvent(rl, 'close'))
-    .bufferCount(10)
-    .bufferTime(1000)
-    .skipWhile((b) => (b || []).length === 0);
+var isListening = true;
+var lines = [];
 
-lines.subscribe(
-    (lines) => {
-        // lock("aws-transmission", (release) => {
-            // promise.create((fulfill, reject) => {
-                console.log("FF: " + lines.map(l => l.toString()).join(",").substr(0, 20))
-        //         fulfill();
-        //     }).then(() => {
-        //         release();
-        //     })
-        // })
-    },
-    err => console.log("FF_Error: %s", err),
-    () => console.log("FF_COMPLETED")
-);
+rl.on("line", l => lines.push(l));
+rl.on("close", () => isListening = false);
 
 console.log("FF_AWS_REGION", process.env.AWS_REGION)
