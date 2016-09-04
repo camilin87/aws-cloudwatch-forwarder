e8cd00cffc9e97b58976dfcb8db285f9c1616ab9
camilin87 <camilin87@gmail.com>
2016-09-03 23:50:21 -0400

    isInputClosed is true when there are no lines and the stream was closed

--- spec/lib/inputRepositorySpec.js
+++ spec/lib/inputRepositorySpec.js
@@ -10,16 +10,32 @@ describe("inputRepository", () => {
     var readlineMock = null;
     var createdInterface = null;
     var rlStub = null;
+    var rlCallbacks = null;
 
     beforeEach(() => {
+        rlCallbacks = {};
+        rlStub = {
+            on: (streamName, callback) => {
+                rlCallbacks[streamName] = callback;
+            }
+        };
+
         createdInterface = null;
         readlineMock = {
             createInterface: (info) => {
                 createdInterface = info;
                 return rlStub;
             }
-        }
+        };
     })
+
+    function sendLine(line) {
+        rlCallbacks["line"](line);
+    }
+
+    function closeInputStream() {
+        rlCallbacks["close"]();
+    }
 
     it ("initializes the readline module with the correct parameters", () => {
         inputRepository(readlineMock, processMock);
@@ -31,9 +47,17 @@ describe("inputRepository", () => {
         });
     })
 
-    it ("isInputClosed it false by default", () => {
+    it ("isInputClosed is false by default", () => {
         var repository = inputRepository(readlineMock, processMock);
 
         expect(repository.isInputClosed()).toBe(false);
+    })
+
+    it ("isInputClosed is true when there are no lines and the stream was closed", () => {
+        var repository = inputRepository(readlineMock, processMock);
+
+        closeInputStream();
+
+        expect(repository.isInputClosed()).toBe(true);
     })
 })
