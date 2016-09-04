ff0102e5ec542dec7757acf7deb40dac17e8853d
camilin87 <camilin87@gmail.com>
2016-09-03 23:38:53 -0400

    verifies the readline method gets properly initialized

--- spec/lib/inputRepositorySpec.js
+++ spec/lib/inputRepositorySpec.js
@@ -1,6 +1,33 @@
 var rfr = require("rfr");
 var inputRepository = rfr("lib/inputRepository");
 
-describe("inputRepository", function () {
-    
+describe("inputRepository", () => {
+    var processMock = {
+        stdin: "the std in",
+        stdout: "the std out"
+    }
+
+    var readlineMock = null;
+    var createdInterface = null;
+    var rlStub = null;
+
+    beforeEach(() => {
+        createdInterface = null;
+        readlineMock = {
+            createInterface: (info) => {
+                createdInterface = info;
+                return rlStub;
+            }
+        }
+    })
+
+    it ("initializes the readline module with the correct parameters", () => {
+        inputRepository(readlineMock, processMock);
+
+        expect(createdInterface).toEqual({
+            input: "the std in",
+            output: "the std out",
+            terminal: false
+        });
+    })
 })
