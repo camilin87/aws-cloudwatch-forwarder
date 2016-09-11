3c746ece3284929417c463886e40687116e24d3d
camilin87 <camilin87@gmail.com>
2016-09-11 10:15:46 -0400

    use the new forwarder service in the main

--- main.js
+++ main.js
@@ -1,75 +1,16 @@
 var rfr = require("rfr")
-var promise = require("the-promise-factory")
-var inputRepository = rfr("lib/inputRepository")()
-var forwarderServiceFactory = rfr("lib/forwarderServiceFactory")()
-var forwarderConfigReader = rfr("lib/forwarderConfigReader")()
-var forwarderService = forwarderServiceFactory.create()
+var forwarder = rfr("lib/forwarder")(
+    rfr("lib/inputRepository")(),
+    rfr("lib/forwarderServiceFactory")(),
+    rfr("lib/forwarderConfigReader")()
+)
 
-console.log("FF_AWS_REGION", process.env.AWS_REGION)
-
-var config = forwarderConfigReader.read()
-console.log("FF_CONFIG", config)
-
-forwarderService
-    .init(config)
+forwarder
+    .run()
     .then(() => {
-
-        sendLogs()
-            .then(() => {
-                console.log("FF_COMPLETED");
-                process.exit(0);
-            }, (err) => {
-                console.log(err, err.stack);
-                process.exit(1);
-            });
-
-    }, (err) => {
+        console.log("FF_COMPLETED");
+        process.exit(0);
+    }, err => {
         console.log(err, err.stack);
         process.exit(1);
     })
-
-
-function sendLogs(){
-    return promise.create((fulfill, reject) => {
-        if (inputRepository.isInputClosed()){
-            fulfill();
-        }
-
-        sendAvailableLogLines()
-            .then(() => {
-                sendLogs().then(fulfill, reject);
-            }, reject);
-    });
-}
-
-function sendAvailableLogLines(){
-    return promise.create((fulfill, reject) => {
-        var lines = inputRepository.getLines();
-
-        if (lines.length === 0){
-
-            console.log("X".repeat(30));
-
-            setTimeout(() => {
-                fulfill();
-            }, 2000);
-
-            return;
-        }
-
-        var linesToSend = [];
-
-        if (lines.length < 5000){
-            linesToSend = lines;
-            inputRepository.setLines([]);
-        }
-        else {
-            linesToSend = lines.slice(0, 5000);
-            inputRepository.setLines(lines.slice(5000));
-        }
-
-        forwarderService
-            .send(linesToSend)
-            .then(fulfill, reject)
-    })
-}
