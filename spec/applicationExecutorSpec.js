98e0fd26015dbf6820a68faa26005a29326c9df0
camilin87 <camilin87@gmail.com>
2016-09-30 07:38:17 -0400

    validate the status

--- spec/applicationExecutorSpec.js
+++ spec/applicationExecutorSpec.js
@@ -4,22 +4,35 @@ var applicationExecutor = rfr("lib/applicationExecutor")
 describe("applicationExecutor", ()  =>{
     var executor = null
     var childProcessStub = null
+    var childProcess = null
 
     beforeEach(() => {
+        childProcess = {
+            stdout: "the stdout of a child",
+            stderr: "the stderr of a child",
+            on: () => {}
+        }
+
         childProcessStub = {
             spawn: () => {}
         }
+        spyOn(childProcessStub, "spawn").and.returnValue(childProcess)
 
         executor = applicationExecutor(childProcessStub)
     })
 
     it ("spaws a process", () => {
-        spyOn(childProcessStub, "spawn").and.returnValue({
-            on: () => {}
-        })
-
         executor.run("npm run test --format=json")
 
         expect(childProcessStub.spawn).toHaveBeenCalledWith("npm", ["run test --format=json"], {stdio: "pipe"})
+    })
+
+    it ("returns the correct status", () => {
+        expect(executor.run("npm run test --format=json").getStatus())
+            .toEqual({
+                closed: false,
+                stdout: "the stdout of a child",
+                stderr: "the stderr of a child"
+            })
     })
 })
