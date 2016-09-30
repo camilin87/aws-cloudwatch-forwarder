a7188f7e82a4b10215b19bbea2a0926fbe1f1f64
camilin87 <camilin87@gmail.com>
2016-09-30 15:34:09 -0400

    start implementing a program initializer

---
+++ spec/lib/programInitializerSpec.js
@@ -0,0 +1,43 @@
+var rfr = require("rfr")
+var programInitializer = rfr("lib/programInitializer")
+
+describe("programInitializer", () => {
+    var initializer = null
+
+    var processStub = null
+    var applicationExecutorStub = null
+
+    beforeEach(() => {
+        processStub = {
+            argv: []
+        }
+
+        applicationExecutorStub = {
+            run: () => {}
+        }
+
+        initializer = programInitializer(
+            processStub,
+            null,
+            applicationExecutorStub,
+            null,
+            null
+        )
+    })
+
+    it ("runs the target application", () => {
+        spyOn(applicationExecutorStub, "run")
+        processStub.argv = [
+            "node",
+            "aws-forwarder",
+            "npm",
+            "run",
+            "test",
+            "--format=json"
+        ]
+
+        initializer.init()
+
+        expect(applicationExecutorStub.run).toHaveBeenCalledWith("npm run test --format=json")
+    })
+})
